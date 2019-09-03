227a953d7128ab4ff8acf21fa66013c975e1c726
晨 李 <262126479@qq.com>
2019-09-03 15:46:44 +0800

    Jackie分支首次提交

--- ConsoleApp/ConsoleApp/Person.cs
+++ ConsoleApp/ConsoleApp/Person.cs
@@ -11,5 +11,6 @@ namespace ConsoleApp
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+        public string Address { get; set; }
     }
 }
